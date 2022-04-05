96e72c87ccef4db9e8c39d23f8c5bd8e60924ed1
cornel <ghita.cornel@gmail.com>
2022-04-06 00:09:56 +0300

    better

--- src/main/java/main/SpringBootMain.java
+++ src/main/java/main/SpringBootMain.java
@@ -1,12 +1,24 @@
 package main;
 
+import main.pc.Producer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 @SpringBootApplication
 public class SpringBootMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootMain.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootMain.class, args);
+
+        Producer producer = context.getBean(Producer.class);
+        AtomicInteger counter = new AtomicInteger();
+        while (true) {
+            int value = counter.getAndIncrement();
+            if (value > 1000) break;
+            producer.sendMessage("message " + value);
+        }
     }
 }
