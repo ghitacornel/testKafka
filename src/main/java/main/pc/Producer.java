087027e45a0e9cbe2c01bd6e289389c0bf5a6e38
cornel <ghita.cornel@gmail.com>
2022-04-06 00:00:40 +0300

    better

--- src/main/java/main/pc/Producer.java
+++ src/main/java/main/pc/Producer.java
@@ -1,16 +1,14 @@
 package main.pc;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class Producer {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
-
-    public Producer(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
 
     public void sendMessage(String message) {
         System.out.println("producing message " + message);
