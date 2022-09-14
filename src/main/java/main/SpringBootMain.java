e4eb367e01ca8c95efb86d918be06c4e05ae1037
cornel <ghita.cornel@gmail.com>
2022-09-14 14:51:18 +0300

    better

--- src/main/java/main/SpringBootMain.java
+++ src/main/java/main/SpringBootMain.java
@@ -1,26 +1,38 @@
 package main;
 
+import lombok.RequiredArgsConstructor;
 import main.producer.StringProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Date;
 
 @SpringBootApplication
 public class SpringBootMain {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringBootMain.class, args);
-
-        // play like in PRODUCTION
-        StringProducer producer = context.getBean(StringProducer.class);
-        AtomicInteger counter = new AtomicInteger();
-        while (true) {
-            int value = counter.getAndIncrement();
-            if (value > 1000) break;
-            producer.sendMessage("message " + value);
-        }
-        // and wait for the effect
+        SpringApplication.run(SpringBootMain.class, args);
     }
+
+    @Configuration
+    @EnableScheduling
+    public static class SchedulerConfiguration {
+    }
+
+    @Component
+    @RequiredArgsConstructor
+    public static class Timer {
+        private final StringProducer producer;
+
+        @Scheduled(fixedRate = 1000)
+        public void reportCurrentTime() {
+            producer.sendMessage("message " + new Date());
+        }
+    }
+
+
 }
