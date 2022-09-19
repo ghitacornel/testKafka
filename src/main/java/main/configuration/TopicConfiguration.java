f2b80b85da10220d2171aea5e30f8791f1b3cc0f
cornel <ghita.cornel@gmail.com>
2022-09-19 06:44:37 +0300

    better

--- src/main/java/main/configuration/TopicConfiguration.java
+++ src/main/java/main/configuration/TopicConfiguration.java
@@ -1,20 +1,12 @@
 package main.configuration;
 
-import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class TopicConfiguration {
-
-    @Value(value = "${kafka.bootstrapAddress}")
-    private String bootstrapAddress;
 
     @Value(value = "${kafka.stringTopicName}")
     private String stringTopicName;
@@ -23,19 +15,12 @@ public class TopicConfiguration {
     private String jsonTopicName;
 
     @Bean
-    KafkaAdmin kafkaAdmin() {
-        Map<String, Object> configs = new HashMap<>();
-        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapAddress);
-        return new KafkaAdmin(configs);
-    }
-
-    @Bean
     NewTopic stringTopic() {
         return new NewTopic(stringTopicName, 1, (short) 1);
     }
 
     @Bean
-    NewTopic jsonTopicName() {
+    NewTopic jsonTopic() {
         return new NewTopic(jsonTopicName, 1, (short) 1);
     }
 }
